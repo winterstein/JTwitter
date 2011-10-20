4d14ef78587dfe2cfe6f2c45dfac66ca1b7018a0
Daniel <daniel@winterwell.com>
2011-10-20 17:48:00 +0100

    wip

--- build/BuildJTwitter.java
+++ build/BuildJTwitter.java
@@ -19,7 +19,7 @@ import winterwell.utils.io.FileUtils;
 public class BuildJTwitter extends BuildTask {
 
 	@Override	
-	protected void doTask() throws Exception {
+	public void doTask() throws Exception {
 		// The project directory
 		File base = FileUtils.getWorkingDirectory();
 		File projectDir = base;
