aed92b75d406fe0a1626fd0e2fc39139c0b97be2
Daniel <daniel@winterwell.com>
2011-12-02 12:36:44 +0000

    wip

---
+++ test/winterwell/jtwitter/ecosystem/TopsyTest.java
@@ -0,0 +1,43 @@
+package winterwell.jtwitter.ecosystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import winterwell.jtwitter.ecosystem.Topsy.UrlInfo;
+
+public class TopsyTest {
+
+	@Test
+	public void testUrlInfo() {
+		Topsy topsy = new Topsy();
+		{
+		UrlInfo ui = topsy.getUrlInfo("http://www.winterwell.com/software/jtwitter.php");
+		System.out.println(ui);
+		}
+		// Sadly, Topsy doesn't resolve equivalent links very well
+		// -- the following all end up at the same page, but only www.soda.sh 
+		// seems to score anything :( 
+		{
+			UrlInfo ui = topsy.getUrlInfo("http://www.sodash.com/");
+			System.out.println(ui);
+		}
+		{
+			UrlInfo ui = topsy.getUrlInfo("http://www.soda.sh/");
+			System.out.println(ui);
+		}
+		{
+			UrlInfo ui = topsy.getUrlInfo("http://soda.sh/");
+			System.out.println(ui);
+		}
+		{
+			UrlInfo ui = topsy.getUrlInfo("http://sodash.com/");
+			System.out.println(ui);
+		}
+		{
+			UrlInfo ui = topsy.getUrlInfo("http://bit.ly/8CsKLw");
+			System.out.println(ui);
+		}
+	}
+
+}
