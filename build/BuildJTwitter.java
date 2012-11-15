22d6c65d81a66773d2ebfebe433bdec1cbe017e9
Daniel <daniel@winterwell.com>
2012-11-15 17:28:35 +0000

    sync

--- build/BuildJTwitter.java
+++ build/BuildJTwitter.java
@@ -13,6 +13,7 @@ import winterwell.jtwitter.OAuthSignpostClient;
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterTest;
 import winterwell.utils.io.FileUtils;
+import winterwell.utils.reporting.Log;
 
 
 
@@ -117,10 +118,15 @@ public class BuildJTwitter extends BuildTask {
 				+" http://winterwell.com/software/jtwitter.php");
 		twitter.setStatus("Thanks to jake & Vicent @GitHub for awesome-grade customer service :)");
 
-		Twitter identica = new Twitter("jtwit", TwitterTest.TEST_PASSWORD);
-		identica.setAPIRootUrl("http://identi.ca/api");
-		identica.setStatus("Released a new version of JTwitter Java library: v"+Twitter.version
-				+" http://winterwell.com/software/jtwitter.php");
+		try {
+			Twitter identica = new Twitter("jtwit", TwitterTest.TEST_PASSWORD);
+			identica.setAPIRootUrl("http://identi.ca/api");
+			identica.setStatus("Released a new version of JTwitter Java library: v"+Twitter.version
+					+" http://winterwell.com/software/jtwitter.php");
+		} catch(Exception ex) {
+			// oh well
+			Log.e("build", ex);
+		}
 	}
 
 
