9227d823385ee5b6192b76ae2ef84cb06604b2da
Daniel <daniel@winterwell.com>
2013-02-22 14:11:53 +0000

    TwitterApps bugfix

--- src/winterwell/jtwitter/Twitter.java
+++ src/winterwell/jtwitter/Twitter.java
@@ -627,7 +627,7 @@ public class Twitter implements Serializable {
 	/**
 	 * JTwitter version
 	 */
-	public final static String version = "2.6.10";
+	public final static String version = "2.7.0";
 
 	/**
 	 * The maximum number of characters that a tweet can contain.
