b536773a8f9f0fdb93aa1613df91dfd9ad3559a7
daniel <daniel@winterwell.com>
2013-03-06 03:12:51 +0000

    test

---
+++ test/winterwell/jtwitter/RateLimitTest.java
@@ -0,0 +1,29 @@
+package winterwell.jtwitter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RateLimitTest {
+
+	@Test
+	public void testGetResource() {
+		{
+			String r = RateLimit.getResource("https://api.twitter.com/1.1/direct_messages.json?id=foo");
+			assert r.equals("/direct_messages") : r;
+		}
+		{
+			String r = RateLimit.getResource("https://api.twitter.com/1.1/direct_messages/sent.json?id=foo");
+			assert r.equals("/direct_messages/sent") : r;
+		}
+		{
+			String r = RateLimit.getResource("https://api.twitter.com/1.1/friends/ids.json");
+			assert r.equals("/friends/ids") : r;
+		}
+		{
+			String r = RateLimit.getResource("https://api.twitter.com/1.1/friends/ids/foo/bar.json");
+			assert r.equals("/friends/ids") : r;
+		}
+	}
+
+}
