f613f31d703e5df1d169a40803afa07d8b088100
Daniel Winterstein <daniel@sodash.com>
2017-06-16 10:44:12 +0100

    3.6

--- src/winterwell/jtwitter/Twitter.java
+++ src/winterwell/jtwitter/Twitter.java
@@ -673,7 +673,7 @@ public class Twitter implements Serializable {
 	/**
 	 * JTwitter version
 	 */
-	public final static String version = "3.5.6";
+	public final static String version = "3.6.0";
 
 	/**
 	 * The maximum number of characters that a tweet can contain.
